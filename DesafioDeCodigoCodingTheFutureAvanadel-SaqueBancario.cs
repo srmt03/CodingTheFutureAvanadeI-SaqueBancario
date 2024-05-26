f4badef3da993b67c5d9afe345bed1433015e79f
Samuel Matos <s.rodriguesmt03@gmail.com>
2024-05-26 14:31:16 -0300

    Desafio de Codigo I DIO - .NET Developer

---
+++ DesafioDeCodigoCodingTheFutureAvanadel-SaqueBancario.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Obtendo o Saldo Total da Conta do Cliente
+        //Console.Write("Digite o saldo total da sua conta bancária: ");
+        int saldoTotal = int.Parse(Console.ReadLine());
+        
+        // Obtendo o Valor do Saque
+        //Console.Write("Digite o valor que deseja sacar da sua conta bancária: ");
+        int valorSaque = int.Parse(Console.ReadLine());
+        
+        //TODO: Criar as condições necessárias para impressão da saída, vide tabela de exemplos.
+        
+        // Verifica se o saldo da conta é o suficiente para realizar o Saque
+        if (saldoTotal >= valorSaque) 
+        {
+          // Subtrair o valor do saque do saldo total
+          saldoTotal -= valorSaque;
+          Console.WriteLine($"Saque realizado com sucesso. Novo saldo: {saldoTotal}");
+          //Console.WriteLine("Saldo restante: " + saldoTotal);
+        }
+        else
+        {
+          // Exibir mensagem de Saldo Insuficiente
+          Console.Write("Saldo insuficiente. Saque nao realizado!");
+        }
+    }
+}
